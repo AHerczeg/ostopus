cea691ac651628763d4edba755c4e6da0b1fbabe
Andras <aherczeg@bol.com>
2019-03-01 11:43:21 +0100

    Added query/result.go

---
+++ query/result.go
@@ -0,0 +1,25 @@
+package query
+
+import "encoding/json"
+
+type ResultDTO struct {
+	QueryID   int64
+	DeviceID  string
+	arguments map[string]interface{}
+}
+
+func (r ResultDTO) GetField(field string) interface{} {
+	return r.arguments[field]
+}
+
+func (r ResultDTO) HasField(field string) bool {
+	_, ok := r.arguments[field]
+	return ok
+}
+
+func (r ResultDTO) UnmarshalArguments(rawMessage []byte) error {
+	if err := json.Unmarshal(rawMessage, r.arguments); err != nil {
+		return err
+	}
+	return nil
+}
